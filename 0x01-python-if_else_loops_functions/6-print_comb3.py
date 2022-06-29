e606e1c9167c117123a639d60e526f58272cb9cc
Mohray <mchungajy@gmail.com>
2022-06-28 19:53:16 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for ones in range(0, 10):
     for tens in range(ones + 1, 10):
         if ones != 8 and tens == 9:
-            print("{}{}".format(ones, tens), end=", ")
+            print("{:d}{:d}".format(ones, tens), end=", ")
         else:
-            print("{}{}".format(ones, tens))
+            print("{:d}{:d}".format(ones, tens))
