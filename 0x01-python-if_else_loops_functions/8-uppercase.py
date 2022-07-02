d53f32d313bc57ee2f24ccfac125b94c82ad8276
Mohray <mchungajy@gmail.com>
2022-07-01 22:03:51 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+
+def uppercase(str):
+    for i in str:
+        if i >= 97 and i <= 122:
+            upp_str = i - 32
+        else:
+            upp_str = i
+        print(f"{}".format(upp_str))
