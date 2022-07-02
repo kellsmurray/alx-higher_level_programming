b21ed1634ebad216045d3fe6310261f8fca53925
Mohray <mchungajy@gmail.com>
2022-07-02 00:35:04 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    str_num = str(num)
+    if len(str_num) == 1:
+        print("{}".format(int(str_num)))
+    else:
+        print("{}".format(int(str_num[-1])))
