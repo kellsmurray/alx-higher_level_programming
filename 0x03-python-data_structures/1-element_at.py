f82822f3773ab157a68d12a3f666fadbadc271f8
Mohray <mchungajy@gmail.com>
2022-07-03 13:06:07 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,5 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return ("None")
     else:
-        for i in my _list:
-            print("{}".format(i))
+        print("{}".format(idx))
