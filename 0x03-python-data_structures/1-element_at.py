edc2180a7414ec3c4eb97ef890735081330a9fe4
Mohray <mchungajy@gmail.com>
2022-07-03 13:02:38 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return ("None")
+    else:
+        for i in my _list:
+            print("{}".format(i))
