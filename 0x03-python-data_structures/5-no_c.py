e37ec522919adec9fcbcadb25a1aa401890343b3
Mohray <mchungajy@gmail.com>
2022-07-04 14:28:49 -0700

    edits

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = my_string[]
+    new_string = my_string[:]
     for i in range(len(new_string) - 1):
         if i == 'c' or i == 'C':
             del new_string[i]
