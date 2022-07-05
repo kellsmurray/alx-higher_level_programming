baea786be5d48306cec4de1fb20ff070de5d359f
Mohray <mchungajy@gmail.com>
2022-07-05 01:56:27 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,22 +2,24 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0:
-        tuple_a[0] = 0
-        tuple_a[1] = 0
+        tupl_a[0] = 0
+        tupl_a[1] = 0
     elif len(tuple_a) == 1:
-        tuple_a[0] = tuple_a[0]
-        tuple_a[1] = 0
+        tupl_a[0] = tuple_a[0]
+        tupl_a[1] = 0
     else:
-        return (tuple_a[0], tuple_a[1])
-    
+        tupl_a[0] = tuple_a[0]
+        tupl_a[1] = tuple_a[1]
+
     if len(tuple_b) == 0:
-        tuple_b[0] = 0
-        tuple_b[1] = 0
+        tupl_b[0] = 0
+        tupl_b[1] = 0
     elif len(tuple_b) == 1:
-        tuple_b[0] = tuple_b[0]
-        tuple_b[1] = 0
+        tupl_b[0] = tuple_b[0]
+        tupl_b[1] = 0
     else:
-        return (tuple_a[0], tuple_a[1])
-    
-    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        tupl_b[0] = tuple_b[0]
+        tupl_b[1] = tuple_b[1]
+
+    new_tuple = (tupl_a[0] + tupl_b[0], tupl_a[1] + tupl_b[1])
     print("{:d}".format(new_tuple))
