2ec68edd309bb93c9cbe926594072f5f45ac71bc
Mohray <mchungajy@gmail.com>
2022-07-08 14:48:56 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 
 def complex_delete(a_dictionary, value):
     new_dict = dict(a_dictionary)
-    for k, v in new_dict.items():
+    for k, v in list(new_dict.items()):
         if v == value:
             del new_dict[key]
     return (new_dict)
